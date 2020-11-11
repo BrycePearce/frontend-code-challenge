b545d65a6a264d62bcc8a268c05d8f33f32e6c2a
bpearce@angelo.edu <bpearce@angelo.edu>
2020-11-11 01:54:01 -0600

    header responsiveness / accessibility

--- frontend/src/components/SearchOptions.vue
+++ frontend/src/components/SearchOptions.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="search-options">
-    <input placeholder="Search" />
-    <input placeholder="Search" />
-    <input placeholder="Search" />
+    <input placeholder="Search" type="text" aria-label="Search" />
+    <select aria-label="Type">
+      <option value="" disabled selected hidden>Type</option>
+      <option value="Temp">Temp</option>
+    </select>
+    <div class="filter-btns">
+      <button>
+        <svg height="50" viewBox="0 0 24 24" width="50">
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
+        </svg>
+      </button>
+      <div class="vertical-spacer"></div>
+      <button>
+        <svg height="50" viewBox="0 0 24 24" width="50">
+          <path d="M0 0h24v24H0z" fill="none" />
+          <path
+            d="M4 11h5V5H4v6zm0 7h5v-6H4v6zm6 0h5v-6h-5v6zm6 0h5v-6h-5v6zm-6-7h5V5h-5v6zm6-6v6h5V5h-5z"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -14,18 +33,45 @@ export default {
 
 <style scoped lang="scss">
 @import "@/assets/_variables.scss";
+@import "@/assets/_variables.scss";
 
 .search-options {
   display: grid;
   column-gap: 0.5rem;
   border: none;
+  max-width: 100%;
 
-  input {
-    all: unset;
+  input,
+  select {
+    border: none;
     padding: 0.8rem;
     text-align: left;
     background-color: $base-gray;
+    color: gray;
     margin: 0.2rem 0;
+    outline: none;
+  }
+
+  select {
+    display: block;
+    // position: relative;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    appearance: none;
+    border: none;
+    /*
+     * Render SVG color unto select arrow
+     * https://stackoverflow.com/questions/25477819/scss-variable-in-background-image-with-svg-image-data-uri
+     */
+    @function url-friendly-colour($colour) {
+      @return "%23" + str-slice("#{$colour}", 2, -1);
+    }
+    background: url("data:image/svg+xml;utf8,<svg viewBox='0 0 24 24' fill='#{url-friendly-colour($base-green)}' xmlns='http://www.w3.org/2000/svg'><g><path d='M6 10l5 5 5-5z'/>'/></g></svg>")
+      no-repeat $base-gray;
+    svg {
+      fill: green;
+    }
+    background-position: right 5px top 50%;
   }
 
   @media screen and (min-width: $medium-screen) {
@@ -39,12 +85,35 @@ export default {
 
     :nth-child(2) {
       grid-area: input2;
-      grid-column: 7 / 11;
+      grid-column: 7 / 10;
     }
 
     :nth-child(3) {
       grid-area: input3;
-      grid-column: 11 / 12;
+      grid-column: 10 / -1;
+    }
+  }
+
+  .filter-btns {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    fill: $base-green;
+    height: 100%;
+
+    button {
+      all: unset;
+      cursor: pointer;
+    }
+
+    svg {
+      height: 100%;
+    }
+
+    .vertical-spacer {
+      border-left: 1px solid gray;
+      opacity: 0.2;
+      height: 100%;
     }
   }
 }
